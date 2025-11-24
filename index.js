420925c0cbc7d816d78f9d3fffcc22f2b64fc19e
carolinagnz <gonzalezpatinofrancy@fpllefia.com>
2025-11-24 20:37:28 +0100

    Add images to fitness activities in the activities array

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const actividades = [
-    { nombre: "Yoga Suave", duracion: 60, intensidad: "Baja", foto: "" },
-    { nombre: "Spinning Intensivo", duracion: 45, intensidad: "Alta", foto: "" },
-    { nombre: "CrossFit Funcional", duracion: 50, intensidad: "Alta", foto: "" },
-    { nombre: "Pilates Máquinas", duracion: 55, intensidad: "Media", foto: "" },
-    { nombre: "Zumba Fitness", duracion: 40, intensidad: "Media", foto: "" }
+    { nombre: "Yoga Suave", duracion: 60, intensidad: "Baja", foto: "ttps://img.freepik.com/foto-gratis/chica-bastante-atractiva-haciendo-yoga-habitacion-luminosa_78826-3257.jpg?semt=ais_hybrid&w=740&q=80" },
+    { nombre: "Spinning Intensivo", duracion: 45, intensidad: "Alta", foto: "https://tse1.mm.bing.net/th/id/OIP.adtNCSp3gdGN8K_Aam4I4wHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" },
+    { nombre: "CrossFit Funcional", duracion: 50, intensidad: "Alta", foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmeaJJedejZWQk6KZ5VIoiSU3JDZFm657pCQ&s" },
+    { nombre: "Pilates Máquinas", duracion: 55, intensidad: "Media", foto: "https://media.istockphoto.com/id/1478160381/es/foto/mujer-entrenando-pilates-en-la-cama-reformador-m%C3%A1quina-de-estudio-de-pilates-reformador-para.jpg?s=612x612&w=0&k=20&c=MI7itx7ZP3KvE5cLOc5e_CvJMldl4i3TImbOAUuCEII=" },
+    { nombre: "Zumba Fitness", duracion: 40, intensidad: "Media", foto: "https://media.istockphoto.com/id/618745062/es/foto/grupo-de-personas-en-la-clase-de-baile-urbano.jpg?s=612x612&w=0&k=20&c=oSPVS3Ffp96TSR283Yzhkm7oJ4opFaxp3_6Io0IZNVs=" }
 ]; 
 
 const franjasHorarias = ["09:00", "11:00", "13:00", "17:00", "19:00"];
